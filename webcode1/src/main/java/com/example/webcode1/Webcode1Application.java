2a09681406c47284bc65dad5c79343fbafb7c338
Bogucheese <88266626+Bogucheese@users.noreply.github.com>
2025-12-06 20:01:27 +0800

    Add files via upload

---
+++ webcode1/src/main/java/com/example/webcode1/Webcode1Application.java
@@ -0,0 +1,13 @@
+package com.example.webcode1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webcode1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webcode1Application.class, args);
+    }
+
+}
---
+++ webcode1/src/test/java/com/example/webcode1/Webcode1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webcode1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webcode1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
